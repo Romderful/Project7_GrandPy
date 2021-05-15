144ba4f50cf123dd641ab6bd9a6cd2784c0af4f4
Mindsky <romain.rondet@gmail.com>
2021-05-15 11:08:59 +0200

    Initial commit

---
+++ app/templates/__init__.py
@@ -0,0 +1,1 @@
+"""Templates entry point."""
---
+++ app/views.py
@@ -0,0 +1,17 @@
+"""Views module."""
+
+from flask import Flask
+from flask.templating import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """Return the index page of the application."""
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
