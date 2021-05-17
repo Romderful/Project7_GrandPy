4364c266a77ab81332b6d3f3a7eae4db9d1d3303
Mindsky <romain.rondet@gmail.com>
2021-05-17 19:37:30 +0200

    add about page

--- app/views.py
+++ app/views.py
@@ -13,5 +13,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    """Return the about file."""
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
