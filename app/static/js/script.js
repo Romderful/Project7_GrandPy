dc68c2f5e7828a487a785b9f5ee52d546e04e357
Mindsky <romain.rondet@gmail.com>
2021-05-31 21:15:58 +0200

    create map

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -16,10 +16,33 @@ function sendUserInput(text) {
         url: "/process",
         data: { data: text },
         success: function (response) {
-            console.log(response);
+            chatZone.append('<div id="map"></div>');
+            var platform = new H.service.Platform({
+                apikey: "38g0LWQAVqN6jUp6xEEwQY9HTe2JF_GIr02m207HNYY"
+            });
+            var defaultLayers = platform.createDefaultLayers();
+
+            var map = new H.Map(document.getElementById('map'),
+                defaultLayers.vector.normal.map, {
+                center: { lat: 50, lng: 5 },
+                zoom: 4,
+                pixelRatio: window.devicePixelRatio || 1
+            });
+
+            window.addEventListener('resize', () => map.getViewPort().resize());
+
+            new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+            H.ui.UI.createDefault(map, defaultLayers);
+
+            moveMap(map, response);
         },
         error: function (error) {
             console.log(error);
         }
     })
 };
+
+function moveMap(map, coordinates) {
+    map.setCenter(coordinates);
+    map.setZoom(14);
+}
