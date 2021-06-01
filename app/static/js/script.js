0bce7f9bfe89141387178b38a7b86c25de5ce454
Mindsky <romain.rondet@gmail.com>
2021-06-01 17:45:55 +0200

    refacto jscript

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,6 +1,5 @@
 const userInput = $("#userInput");
 const chatZone = $("#chatZone");
-
 let mapIndex = 0;
 
 $("form").keypress(function (event) {
@@ -17,28 +16,9 @@ function sendUserInput(text) {
         method: "POST",
         url: "/process",
         data: { data: text },
-        success: function (response) {
+        success: function (coordinates) {
             chatZone.append(`<div class="map" id="map${mapIndex}"></div>`);
-            let platform = new H.service.Platform({
-                apikey: "38g0LWQAVqN6jUp6xEEwQY9HTe2JF_GIr02m207HNYY"
-            });
-            let defaultLayers = platform.createDefaultLayers();
-
-            let map = new H.Map(document.getElementById(`map${mapIndex}`),
-                defaultLayers.vector.normal.map, {
-                center: { lat: 50, lng: 5 },
-                zoom: 4,
-                pixelRatio: window.devicePixelRatio || 1
-            });
-
-            window.addEventListener('resize', () => map.getViewPort().resize());
-
-            new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
-            H.ui.UI.createDefault(map, defaultLayers);
-
-            moveMap(map, response);
-
-            mapIndex++;
+            createMap(coordinates);
         },
         error: function (error) {
             console.log(error);
@@ -46,7 +26,21 @@ function sendUserInput(text) {
     })
 };
 
-function moveMap(map, coordinates) {
+function createMap(coordinates) {
+    let platform = new H.service.Platform({
+        apikey: "38g0LWQAVqN6jUp6xEEwQY9HTe2JF_GIr02m207HNYY"
+    });
+    let defaultLayers = platform.createDefaultLayers();
+    let map = new H.Map(document.getElementById(`map${mapIndex}`),
+        defaultLayers.vector.normal.map, {
+        center: { lat: 50, lng: 5 },
+        zoom: 4,
+        pixelRatio: window.devicePixelRatio || 1
+    });
+    window.addEventListener('resize', () => map.getViewPort().resize());
+    new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+    H.ui.UI.createDefault(map, defaultLayers);
     map.setCenter(coordinates);
     map.setZoom(14);
+    mapIndex++;
 }
